a35acc1bfcc97638efd0fec5c83cdf3fe3040c84
Lafoerte91 <lafoerte@gmail.com>
2025-02-17 08:44:34 +0300

    PropTypes

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-
+import PropTypes from 'prop-types';
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -39,8 +39,6 @@ class CharInfo extends Component {
             .getCharacter(charId)
             .then(this.onCharLoaded)
             .catch(this.onError);
-
-        this.foo.bar = 0
     }
 
     onCharLoaded = (char) => {
@@ -126,6 +124,10 @@ const View = ({char}) => {
             </ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charId: PropTypes.number
 }
 
 export default CharInfo;
