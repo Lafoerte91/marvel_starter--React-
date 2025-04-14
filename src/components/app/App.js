8e9aa01ebcda918f74206b29a3f52ccd13686467
Lafoerte91 <lafoerte@gmail.com>
2025-04-14 12:33:13 +0300

    практика

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,107 +1,46 @@
-// import { useState } from "react";
-// import AppHeader from "../appHeader/AppHeader";
-// import RandomChar from "../randomChar/RandomChar";
-// import CharList from "../charList/CharList";
-// import CharInfo from "../charInfo/CharInfo";
-// import decoration from '../../resources/img/vision.png';
-// import ErrorBoundary from '../errorBoundary/ErrorBoundary'
+import { useState } from "react";
+import AppHeader from "../appHeader/AppHeader";
+import RandomChar from "../randomChar/RandomChar";
+import CharList from "../charList/CharList";
+import CharInfo from "../charInfo/CharInfo";
+import decoration from '../../resources/img/vision.png';
+import ErrorBoundary from '../errorBoundary/ErrorBoundary'
 
-// const App = () => {
-//     const [selectedChar, setChar] = useState(null)
+const App = () => {
+    const [selectedChar, setChar] = useState(null)
 
-//     const onCharSelected = (id) => {
-//         setChar(id)
-//     }
-
-//         return (
-//             <div className="app">
-//                 <AppHeader/>
-//                 <main>
-//                     <ErrorBoundary>
-//                         <RandomChar/>
-//                     </ErrorBoundary>
-//                     <div className="char__content">
-//                         <ErrorBoundary>
-//                             <CharList onCharSelected={onCharSelected}/>
-//                         </ErrorBoundary>
-//                         <ErrorBoundary>
-//                             <CharInfo charId={selectedChar}/>
-//                         </ErrorBoundary>
-//                     </div>
-//                     <img className="bg-decoration" src={decoration} alt="vision"/>
-//                 </main>
-//             </div>
-//         )
-//  }
-
-// export default App;
-
-
-
-
-
-
-
-
-
-
-
-import { useState, useEffect} from 'react';
-import {Container} from 'react-bootstrap';
-import './App.css';
-
-function useInputWithValidate(initialValue) {
-    const [value, setValue] = useState(initialValue)
-    const onChange = (e) => {
-        setValue(e.target.value)
+    const onCharSelected = (id) => {
+        setChar(id)
     }
-    const validateInput = () => {
-        return value.search(/\d/) >= 0 
-    }
-    return {value, onChange, validateInput}
-}
 
-const Form = () => {
-    const input = useInputWithValidate('')
-    const textArea = useInputWithValidate('')
-    const color = input.validateInput() ? 'text-danger' : null
-
-    return (
-        <Container>
-            <form className="w-50 border mt-5 p-3 m-auto">
-                <div className="mb-3">
-                    <input value={`${input.value} / ${textArea.value}`} type="text" className="form-control" readOnly/>
-                    <br />
-                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                    <br />
-                    <input 
-                        onChange={input.onChange} 
-                        type="email" 
-                        value = {input.value}
-                        className={`form-control ${color}`}
-                        id="exampleFormControlInput1" 
-                        placeholder="name@example.com"/>
+        return (
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <ErrorBoundary>
+                        <RandomChar/>
+                    </ErrorBoundary>
+                    <div className="char__content">
+                        <ErrorBoundary>
+                            <CharList onCharSelected={onCharSelected}/>
+                        </ErrorBoundary>
+                        <ErrorBoundary>
+                            <CharInfo charId={selectedChar}/>
+                        </ErrorBoundary>
                     </div>
-                    <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <br />
-                    <textarea 
-                        onChange = {textArea.onChange}
-                        value={textArea.value}
-                        className="form-control" 
-                        id="exampleFormControlTextarea1" 
-                        rows="3">
-                    </textarea>
-                </div>
-            </form>
-        </Container>
-    )
-}
-
-function App() {
-    return (
-        <Form/>
-    );
-}
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        )
+ }
 
 export default App;
+
+
+
+
+
+
+
+
+
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,32 +1,24 @@
 import {useState, useEffect, useRef} from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
-import MarvelService from '../../services/MarvelService';
+import useMarvelService from '../../services/MarvelService';
 import './charList.scss';
 
 const CharList = (props) => {
     const [charList, setCharList] = useState([])
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(false)
     const [newItemLoading, setNewItemLoading] = useState(false)
     const [offset, setOffset] = useState(210)
     const [charEnded, setCharEnded] = useState(false)
-    
-    const marvelService = new MarvelService();
+    const {loading, error, getAllCharacters} = useMarvelService();
 
     useEffect(() => {
-        onRequest()
+        onRequest(offset, true)
     }, [])
 
-    const onRequest = (offset) => {
-        onCharListLoading();
-        marvelService.getAllCharacters(offset)
+    const onRequest = (offset, initial) => {
+        initial ? setNewItemLoading(false) : setNewItemLoading(true)
+        getAllCharacters(offset)
             .then(onCharListLoaded)
-            .catch(onError)
-    }
-
-    const onCharListLoading = () => {
-        setNewItemLoading(true)
     }
 
     const onCharListLoaded = (newCharList) => {
@@ -36,27 +28,21 @@ const CharList = (props) => {
         }
 
         setCharList(charList => [...charList, ...newCharList])
-        setLoading(loading => false)
         setNewItemLoading(newItemLoading => false)
         setOffset(offset => offset + 9)
         setCharEnded(charEnded => ended)
     }
 
-    const onError = () => {
-        setError(true)
-        setLoading(loading => false)
-    }
-
     const itemRefs = useRef([])
 
-    focusOnItem = (id) => {
+    const focusOnItem = (id) => {
         itemRefs.current.forEach(item => item.classList.remove('char__item_selected'))
         itemRefs.current[id].classList.add('char__item_selected')
         itemRefs.current[id].focus()
     }
 
     function renderItems(arr) {
-        const items =  arr.map((item) => {
+        const items =  arr.map((item, i) => {
             let imgStyle = {'objectFit' : 'cover'};
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = {'objectFit' : 'unset'};
@@ -94,14 +80,13 @@ const CharList = (props) => {
 
     const items = renderItems(charList);
     const errorMessage = error ? <ErrorMessage/> : null;
-    const spinner = loading ? <Spinner/> : null;
-    const content = !(loading || error) ? items : null;
+    const spinner = loading && !newItemLoading ? <Spinner/> : null;
 
     return (
         <div className="char__list">
             {errorMessage}
             {spinner}
-            {content}
+            {items}
             <button 
                 className="button button__main button__long"
                 disabled={newItemLoading}
