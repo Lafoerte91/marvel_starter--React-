379ed58a5f271c7ef1512ff9d1ff005a8cba250d
Lafoerte91 <lafoerte@gmail.com>
2025-04-14 18:23:40 +0300

    +

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,46 +1,29 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; 
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import decoration from '../../resources/img/vision.png';
-import ErrorBoundary from '../errorBoundary/ErrorBoundary'
-
+import MainPage from "../pages/MainPage";
+import ComicsPage from "../pages/ComicsPage";
 const App = () => {
-    const [selectedChar, setChar] = useState(null)
-
-    const onCharSelected = (id) => {
-        setChar(id)
-    }
-
-        return (
+    return (
+        <Router>
             <div className="app">
-                <AppHeader/>
+                <AppHeader />
                 <main>
-                    <ErrorBoundary>
-                        <RandomChar/>
-                    </ErrorBoundary>
-                    <div className="char__content">
-                        <ErrorBoundary>
-                            <CharList onCharSelected={onCharSelected}/>
-                        </ErrorBoundary>
-                        <ErrorBoundary>
-                            <CharInfo charId={selectedChar}/>
-                        </ErrorBoundary>
-                    </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                    <Routes> {/* Изменено на Routes */}
+                        <Route path="/" element={
+                            <>
+                                <MainPage />
+                            </>
+                        } />
+                        <Route path="/comics" element={
+                            <>
+                                <ComicsPage />
+                            </>
+                        } />
+                    </Routes>
                 </main>
             </div>
-        )
- }
+        </Router>
+    );
+};
 
 export default App;
-
-
-
-
-
-
-
-
-
