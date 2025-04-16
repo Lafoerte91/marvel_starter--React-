df2501703c848c891c0d923b8f504c7e5b321272
Lafoerte91 <lafoerte@gmail.com>
2025-04-16 21:23:31 +0300

    lazy

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -82,6 +82,12 @@ const CharList = (props) => {
     const errorMessage = error ? <ErrorMessage/> : null;
     const spinner = loading && !newItemLoading ? <Spinner/> : null;
 
+    if(loading) {
+        import('./someFunc')
+            .then(obj => obj.default())
+            .catch()
+    }
+
     return (
         <div className="char__list">
             {errorMessage}
