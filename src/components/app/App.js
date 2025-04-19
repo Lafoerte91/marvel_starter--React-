51ac6632a35b7427b88ff89771c7493923d56050
Lafoerte91 <lafoerte@gmail.com>
2025-04-19 14:13:09 +0300

    useReducer

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -48,64 +48,62 @@
 
 
 
-import {useState, memo, Component, createContext, useContext} from 'react';
+
+import {useState, useReducer} from 'react';
 import {Container} from 'react-bootstrap';
 import './App.css';
 
+function reducer(state, action) {
+    switch(action.type) {
+        case 'toggle':
+            return {autoplay: !state.autoplay}
+        case 'slow':
+            return {autoplay: 300}
+        case 'fast':
+            return {autoplay: 700}
+        default:
+            throw new Error('')
+    }
+}
 
-const dataContext = createContext({
-    mail: "name@example.com",
-    text: 'some text'
-})
+const Slider = () => {
+    const [slide, setSlide] = useState(0);
+    const [autoplay, dispatch] = useReducer(reducer, {autoplay: false});
 
-const {Provider, Consumer} = dataContext
+    function changeSlide(i) {
+        setSlide(slide => slide + i);
+    }
 
-const Form = memo((props) => {
     return (
         <Container>
-            <form className="w-50 border mt-5 p-3 m-auto">
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label mt-3">Email address</label>
-                    <InputComponent />
-                    </div>
-                    <div className="mb-3">
-                    <label htmlFor="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-                    <textarea value={props.text} className="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <div className="slider w-50 m-auto">
+                <img className="d-block w-100" src="https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg" alt="slide" />
+                <div className="text-center mt-5">Active slide {slide} <br/>{autoplay.autoplay ? 'auto' : null} </div>
+                <div className="buttons mt-3">
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(-1)}>-1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => changeSlide(1)}>+1</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'toggle'})}>toggle autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'slow'})}>slow autoplay</button>
+                    <button 
+                        className="btn btn-primary me-2"
+                        onClick={() => dispatch({type: 'fast'})}>fast autoplay</button>
                 </div>
-            </form>
+            </div>
         </Container>
-    )
-})
-
-const InputComponent = () => {
-    const context = useContext(dataContext)
-    return(
-        <input 
-            value={context.mail} 
-            type="email" 
-            className='form-control' 
-            placeholder="name@example.com" 
-        />
     )
 }
 
 function App() {
-    const [data, setData] = useState({
-        mail: "name@example.com",
-        text: 'some text'
-    });
-
     return (
-        <Provider value={data}>
-            <Form text={data.text}/>
-            <button 
-                onClick={() => setData({
-                    mail: "second@example.com",
-                    text: 'another text'
-                })}>
-                Click me
-            </button>
-        </Provider>
+        <Slider/>
     );
 }
 
